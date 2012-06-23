36ecd38acf990eca57e1bd39a5d698889bd349ff
r1c4p3 <jeffmiller18@gmail.com>
2012-06-23 06:39:07 -0700

    First step

--- src/Reader.java
+++ src/Reader.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Reader {
 	File directory;
-	File[] manafests;
+	RingBufferReaer[] manafests;
 	
 	public Reader(){
 		if(System.getProperty("os.name").contains("Windows"))
