27544a908df28479c5528ffb7d28e85baa4ba28d
Ethan Steinberg <ethan.steinberg@gmail.com>
2012-06-23 07:00:43 -0700

    Start of ethan branch

--- src/Main.java
+++ src/Main.java
@@ -14,26 +14,10 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
-		try {
-			Class.forName("org.h2.Driver");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 		
-		try {
-			Connection c = DriverManager.getConnection("jdbc:h2:~/test2","","");
-			
-			Statement stat = c.createStatement();
-			ResultSet res = stat.executeQuery("SELECT * from foo");
-			res.next();
-			System.out.println(res.getString("day"));
-			c.close();
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		
+		Database b = new Database();
 	
 
 	}
