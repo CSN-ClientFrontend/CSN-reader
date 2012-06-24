21d71a6df974f2c4011e53106a67bb418d53f3d1
r1c4p3 <jeffmiller18@gmail.com>
2012-06-23 21:58:09 -0700

    First commit

--- src/RingBufferReaer.java
+++ src/RingBufferReaer.java
@@ -1,9 +1,26 @@
 import java.io.*;
+
+import javax.imageio.stream.FileImageInputStream;
 public class RingBufferReaer {
 	File[] files;
-	public final static int manafest = 3;
+	FileInputStream[] input;
+	public final static int metadata = 3;
+	public final static int timecode = 4;
 	
-	public RingBufferReaer(int n){
+	public RingBufferReaer(int n,File directory) throws FileNotFoundException{
+		files = new File[] {new File(directory,n+"-0"),new File(directory,n+"-1"),
+					new File(directory,n+"-2"),new File(directory,n+"-metadata"),
+					new File(directory,n+"-timecode")};
+		input = new FileInputStream[files.length];
+		input[metadata] = new FileInputStream(files[metadata]);
+//		for(int i=0;i<files.length-1;i++)
+//			try {
+//				input[i] = new FileImageInputStream(files[i]);
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+		
 		
 	}
 
